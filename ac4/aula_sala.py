856bf7a1fcd3505e676708f082d924139b256ce5
Igor Laur <igor.laur2@gmail.com>
2019-03-27 17:52:48 -0300

    POST funcionando, GET com bug

--- ac4/aula_sala.py
+++ ac4/aula_sala.py
@@ -9,6 +9,7 @@ database['PROFESSOR'] = []
 @app.route('/alunos', methods = ['POST'])
 def novo_aluno():
     novo_aluno = request.get_json()
+    print(novo_aluno)
     database['ALUNO'].append(novo_aluno)
     return jsonify(database['ALUNO'])
 
@@ -25,6 +26,11 @@ def professores():
     return jsonify(database['PROFESSOR'])
 
 @app.route('/alunos/<int:id_aluno>', methods = ['GET'])
+def localiza_aluno(id_aluno):
+    for aluno in database['ALUNO']:
+        if aluno['id'] == id_aluno:
+            return jsonify(aluno)
+    return '', 404
 
 if __name__ == '__main__':
     app.run()
